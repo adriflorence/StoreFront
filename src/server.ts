f5d9f5f4166fa47cb83f9fff8148926b0ff2b043
Adri Forczek <forczekadri@gmail.com>
2021-02-01 22:02:02 +0000

    feature: adds post endpoint for users and sets up cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
 
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
+})
+
+app.post('/users/create', function (req, res) {
+    res.send("Successful post request by " + req.body.firstName)
 })
 
 app.listen(3000, function () {
