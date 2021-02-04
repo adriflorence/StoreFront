fccad1540c935a38329337a1a8da9bfa7388db75
Adri Forczek <forczekadri@gmail.com>
2021-02-04 22:50:14 +0000

    feature: defines routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import { routes } from './api/routes';
+
 export const app: express.Application = express();
 
 // parse requests of content-type application/json
@@ -9,3 +11,6 @@ app.use(bodyParser.json())
 
 // enable cors
 app.use(cors());
+
+// define routes
+routes(app);
