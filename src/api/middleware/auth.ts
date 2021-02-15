cfe3aad1aa43adccc73e2895e019d526feb43daa
Adri Forczek <forczekadri@gmail.com>
2021-02-15 22:00:01 +0000

    feature: completes authToken verification

--- src/api/middleware/auth.ts
+++ src/api/middleware/auth.ts
@@ -3,7 +3,13 @@ import jsonwebtoken from 'jsonwebtoken';
 
 export const authToken = (req: Request, res: Response, next: NextFunction) => {
     try {
-        // TODO
+        const authHeader = req.headers.authorization;
+        const jwttoken = authHeader ? authHeader.split(' ')[1] : null;
+        if(jwttoken == null) {
+            return res.status(401);
+        }
+        const decoded = jsonwebtoken.verify(jwttoken, process.env.JWT_SECRET as string);
+        res.locals.authToken = decoded;
         next();
     } catch (error) {
         error.code = 401;
