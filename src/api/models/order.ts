fded417e76a6687e5c761c17dacddb3a2d53aef8
Adri Forczek <forczekadri@gmail.com>
2021-02-16 22:59:46 +0000

    fix: typo in request param

--- src/api/models/order.ts
+++ src/api/models/order.ts
@@ -3,7 +3,7 @@ import { database } from '../database/database';
 
 // get orders by user id
 export const getOrdersByUserId = (req: Request, res: Response) => {
-    const user_id = parseInt(req.params.id);
+    const user_id = parseInt(req.params.user_id);
     database.query(`SELECT * FROM orders WHERE user_id = $1`, [user_id], (error, orders) => {
         if (error) {
             throw error
@@ -12,15 +12,3 @@ export const getOrdersByUserId = (req: Request, res: Response) => {
         }
     });
 };
-
-// get order by id
-export const getOrderById = (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    database.query(`SELECT * FROM orders WHERE id = $1`, [id], (error, order) => {
-        if (error) {
-            throw error
-        } else {
-            res.json(order);
-        }
-    });
-}
